edac65e66e1051340226438b87f0c3df40b344b6
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2022-09-26 19:17:56 +0800

    Fix ESLint Problems

--- contact.frontend/src/containers/SignInPage.js
+++ contact.frontend/src/containers/SignInPage.js
@@ -1,19 +1,13 @@
-import React, { useState} from "react";
-import { Form, Button } from "react-bootstrap";
+import React from "react";
 import GoogleIcon from "@mui/icons-material/Google";
-import { useDispatch } from "react-redux";
-import { SignIn } from "../services/authentication";
-import { NavLink } from "react-router-dom";
+
 import { ToastContainer } from "react-toastify";
+import SignInForm from "../components/Form/SignInForm";
 
 export default function SignInPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-
   return (
     <div className="container">
-    <ToastContainer />
+      <ToastContainer />
       <div className="row">
         <div className="offset-md-2 col-lg-5 col-md-7 offset-lg-4 offset-md-3 p-5">
           <div className="panel border bg-white">
@@ -21,36 +15,7 @@ export default function SignInPage() {
               <h3 className="text-center">Login</h3>
             </div>
             <div className="panel-body p-3">
-              <Form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  SignIn(dispatch, { username, password });
-                }}
-              >
-                <Form.Group className="mb-3">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control
-                    placeholder="Username"
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                  Sign In
-                </Button>
-                <div className="text-center pt-4 text-muted">
-                  Don't have an account?  <NavLink to="/signup"> Sign Up</NavLink>
-                </div>
-              </Form>
+              <SignInForm />
             </div>
             <div className="mx-3 my-2 py-2 bordert">
               <div className="text-center py-3">
